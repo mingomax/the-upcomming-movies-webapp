a436edb662b39a7823a3f06c1083f516e0478fd9
Domingos Teruel <mingomax@dteruel.net.br>
2019-08-29 13:17:31 -0300

    Organized structure of page and their interactions. Add some styles from boilerplate to facily things

--- frontend/app/src/app/app.component.ts
+++ frontend/app/src/app/app.component.ts
@@ -1,9 +1,11 @@
-
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
 
 import { slideInAnimation } from '@helpers/animation.helper';
 import { environment } from '@env/environment';
+import { MoviesService } from './movies/movies.service';
+import { PersistenceService } from './services/persistence.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +17,24 @@ import { environment } from '@env/environment';
   animations: [slideInAnimation]
 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = `${environment.title}`;
+
+  constructor(
+    private router: Router,
+    private moviesService: MoviesService,
+    private persister: PersistenceService
+  ) {}
+
+  ngOnInit(): void {
+    this.moviesService
+    .listGenres()
+    .pipe(tap(console.log))
+    .subscribe(data => this.persister.set('genres', data));
+    this.router.navigate(['/movies']);
+  }
 
   getAnimationData(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
   }
 }
-
