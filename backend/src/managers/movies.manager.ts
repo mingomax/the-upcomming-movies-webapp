999cf470aabb6264ee64c92d9f2ad63e7199ed65
Domingos Teruel <mingomax@dteruel.net.br>
2019-08-29 13:11:28 -0300

    Implements List of Movies and Genres from TMDB

--- backend/src/managers/movies.manager.ts
+++ backend/src/managers/movies.manager.ts
@@ -5,12 +5,26 @@ import tmdbService from '../services/tmdb.service';
 
 class MoviesManager {
 
+  static movieById(request: Request, response: Response) {
+    const { id } = request.params;
+
+    return tmdbService.movieById(Number(id))
+      .then(data => response.status(httpStatus.OK).json(data))
+      .catch(error => console.error.bind(console, `Request error: ${error}`));
+  }
+
   static listMovies(request: Request, response: Response) {
     const { page } = request.query || 1;
     const { lang } = request.query || 'en-US';
     const { query } = request. query || -1;
     return tmdbService.upcomingMoviesList(lang, page)
       .then(data => response.status(httpStatus.OK).json(data))
+      .catch(error => console.error.bind(console, `Request error: ${error}`));
+  }
+
+  static listGenres(request: Request, response: Response) {
+    return tmdbService.genresMovieList()
+      .then((result: any) => response.status(httpStatus.OK).json({ items: result.data }))
       .catch(error => console.error.bind(console, `Request error: ${error}`));
   }
 }
