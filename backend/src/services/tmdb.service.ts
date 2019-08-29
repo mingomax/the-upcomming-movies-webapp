4c35b745db1bbb6b5b6b636f5e1682c6e431a20c
Domingos Teruel <mingomax@dteruel.net.br>
2019-08-29 13:11:28 -0300

    Implements List of Movies and Genres from TMDB

--- backend/src/services/tmdb.service.ts
+++ backend/src/services/tmdb.service.ts
@@ -3,7 +3,7 @@ import config  from '../config/config';
 
 class TmdbService {
   options = {
-    uri:  `${config.tmdb.uri}/movie/upcoming`,
+    uri:  `${config.tmdb.uri}/movie`,
     qs: {
       'api-key': config.tmdb.secret,
     },
@@ -12,9 +12,21 @@ class TmdbService {
 
   constructor() { }
 
+  async movieById(id: number) {
+    console.log(id);
+    return await axios.get(`${this.options.uri}/${id}`, {
+      headers: this.options.headers,
+      params: {
+        'api_key': config.tmdb.secret,
+      },
+    })
+    .then(response => response.data)
+    .catch(error => console.error(error));
+  }
+
   async upcomingMoviesList(language: string = 'en-US', page: number = 1) {
     const qs = Object.assign(this.options.qs, { language, page });
-    const data = await axios.get(this.options.uri, {
+    const data = await axios.get(`${this.options.uri}/upcoming`, {
       headers: { 'Content-Type':'application/json' },
       params: this.options.qs,
     })
@@ -26,6 +38,26 @@ class TmdbService {
     })
     .catch(error => console.error(error))
     .then(data => data);
+    return data;
+  }
+
+  async genresMovieList() {
+    const data = await axios.get(
+      `${config.tmdb.uri}/genre/movie/list`,
+      {
+        headers: this.options.headers,
+        params: {
+          'api_key': config.tmdb.secret,
+        },
+      })
+      .then((responseBody: any) => {
+        return {
+          data: responseBody.data.genres,
+        };
+      })
+      .catch(error => console.error(error))
+      .then(data => data);
+
     return data;
   }
 }
